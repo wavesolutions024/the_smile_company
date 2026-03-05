305dd7a8d19342e6372df18c6f47680bc3eb7af1
rishabhkhade <89901588+rishabhkhade@users.noreply.github.com>
2026-03-05 15:15:00 +0530

    pp

--- src/comp/page_top/PageTop.jsx
+++ src/comp/page_top/PageTop.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import "./PageTop.scss";
 
 const PageTop = () => {
   return (
-    <div>PageTop</div>
-  )
-}
+    <>
+      <div className="parent pagetop_parent">
+        <div className="cont pagetop_cont">
+          <div className="left_pagetop">
+            <h1>About Us</h1>
+          </div>
+          <div className="right_pagetop">right</div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default PageTop
+export default PageTop;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import "./Home.scss"
 import HeroSection from '../../comp/Hero_section/HeroSection'
+import PageTop from '../../comp/page_top/PageTop'
 const Home = () => {
   return (
     < >
-     <HeroSection/>
+     {/* <HeroSection/> */}
+     <PageTop />
     </>
   )
 }
