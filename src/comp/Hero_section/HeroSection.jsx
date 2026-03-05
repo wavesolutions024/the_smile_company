eb4aa7ef202ae02d6405dd84d8ac4990eaa8e0ee
Ketan Dudka <ketandudka566123@gmail.com>
2026-03-05 14:35:40 +0530

    pune

---
+++ src/comp/Hero_section/HeroSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./HeroSection.scss"
+const HeroSection = () => {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default HeroSection
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-
+import "./Home.scss"
 const Home = () => {
   return (
     < >
       <div class="home_parent parent">
         <div class="home_cont cont">
-            
+          
+
         </div>
       </div>
     </>
