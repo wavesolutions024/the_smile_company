6f87dc2408efc6f719b457c55e3a218a38233da9
Ketan Dudka <ketandudka566123@gmail.com>
2026-03-06 18:48:35 +0530

    push

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Home.scss";
 import HeroSection from "../../comp/Hero_section/HeroSection";
 import Button from "../../comp/button/Button";
 import abt_img from "../../assets/about_section/img5.webp";
 import img5 from "../../assets/about_section/img4.webp";
+import img4 from "../../assets/about_section/img5.webp";
 import icon1 from "../../assets/about_section/medical-symbol.png";
 import icon2 from "../../assets/about_section/medicine.png";
 import icon3 from "../../assets/about_section/doctor.png";
+
 const Home = () => {
+  const [accordian, setAccordian] = useState(0);
+  const accordianContent = [
+    {
+      title: "Expertise in Dental Industry",
+      description:
+        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a making it look like readable English.",
+    },
+    {
+      title: "Expertise in Dental Industry",
+      description:
+        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a making it look like readable English.",
+    },
+    {
+      title: "Expertise in Dental Industry",
+      description:
+        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a making it look like readable English.",
+    },
+  ];
   return (
     <>
       <HeroSection />
@@ -80,6 +100,66 @@ const Home = () => {
                 <div class="icon">
                   <img src={icon3} alt="" />
                 </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="why_choose_parent bg-img-cover parent">
+        <div class="why_choose_cont cont">
+          <div class="wc_left">
+            <h1>Why Choose Dental Care</h1>
+            <p>
+              It is a long established fact that a reader will be distracted by
+              the readable content of a page when looking at its layout.
+            </p>
+            <div class="accordian">
+              {accordianContent?.map((item, index) => (
+                <div class="accordian_item">
+                  <div
+                    class="accordian_title"
+                    onClick={() => setAccordian(index)}
+                  >
+                    <h1> {item.title} </h1>
+                    <div class="count"> {index + 1} </div>
+                  </div>
+                  {accordian === index && (
+                    <div
+                      class={
+                        accordian === index
+                          ? "accordian_desc active"
+                          : "accordian_desc"
+                      }
+                    >
+                      <div class="left">
+                        <h1> {item.title} </h1> <p>{item.description}</p>
+                      </div>
+                      <div class="rg_image">
+                        <img src={img4} alt="" />
+                      </div>
+                    </div>
+                  )}
+                </div>
+              ))}
+            </div>
+          </div>
+          <div class="wc_right">
+           
+            <div class="top_rg">
+              <div class="rg_tp_lg">
+                <div class="lg1 bg-img-contain">
+                  <img src={img5} alt=""/>
+                </div>
+                <div class="lg2 bg-img-cover"></div>
+              </div>
+              <div class="rp_tp_rg bg-img-cover"></div>
+            </div>
+            <div class="bottom_rg">
+              <div class="bt_lg bg-img-cover">
+
+              </div>
+              <div class="bt_rg">
+                <div class="inner bg-img-cover"></div>
               </div>
             </div>
           </div>
