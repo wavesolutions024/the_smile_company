8c7526ab784b866a4df62c7a293031fd5413361f
Ketan Dudka <ketandudka566123@gmail.com>
2026-03-06 12:45:56 +0530

    push

--- src/comp/header/Header.jsx
+++ src/comp/header/Header.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import { useState } from "react";
 import "./Header.scss";
 import logo from "../../assets/header/header_logo.png";
 import Button from "../button/Button";
+import { RxCross2 } from "react-icons/rx";
+import { Link } from "react-router-dom";
+import { FaWhatsapp } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
 const Header = () => {
+  const [header, setHeader] = useState(false);
   return (
     <>
       <div class="header_parent parent">
@@ -12,12 +18,60 @@ const Header = () => {
           </div>
           <div class="header_right">
             <div class="header_btn">
-              <Button/>
+              <Button />
             </div>
-            <div class="hamburger">
+
+            <div class="hamburger" onClick={() => setHeader(true)}>
               <span></span>
               <span></span>
               <span></span>
+            </div>
+          </div>
+        </div>
+
+        <div class={header ? "header_section active" : "header_section"}>
+          <div
+            class={
+              header
+                ? "inner_header_section inner_active"
+                : "inner_header_section"
+            }
+          >
+            <div class="top_section">
+              <div class="logo">
+                <img src={logo} alt="" />
+              </div>
+
+              <div class="cross_button" onClick={()=>setHeader(false)}>
+                <RxCross2 />
+              </div>
+            </div>
+
+            <div class="bottom_hed_section">
+              <Link to="/">
+                Home
+              </Link>
+              <Link to="/">
+                About Us
+              </Link>
+              <Link to="/">
+                Services
+              </Link>
+              <Link to="/">
+                Contact
+              </Link>
+
+              <div class="social_icons">
+                <a href="#">
+                  <FaWhatsapp />
+                </a>
+                <a href="#">
+                 <FaFacebook />
+                </a>
+                <a href="#">
+                 <FaInstagram />
+                </a>
+              </div>
             </div>
           </div>
         </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import "./Home.scss";
 import HeroSection from "../../comp/Hero_section/HeroSection";
 import Button from "../../comp/button/Button";
+import abt_img from "../../assets/about_section/img5.webp";
+import img5 from "../../assets/about_section/img4.webp";
+import icon1 from "../../assets/about_section/medical-symbol.png";
+import icon2 from "../../assets/about_section/medicine.png";
+import icon3 from "../../assets/about_section/doctor.png";
 const Home = () => {
   return (
     <>
@@ -9,20 +14,74 @@ const Home = () => {
 
       <div class="about_section_parent bg-img-cover parent">
         <div class="about_section_cont cont">
-          <div class="ab_left">
-            <div class="image">
-              
+          <div class="abt_top_sec">
+            <div class="ab_left">
+              <div class="image">
+                <img src={abt_img} alt="" />
+
+                <div class="round_image">
+                  <img src={img5} alt="" />
+                </div>
+              </div>
+            </div>
+            <div class="ab_right">
+              <h1>We Care About Your Dental Health</h1>
+              <p>
+                It is a long established fact that a reader will be distracted
+                by the readable content of a page when looking at its layout.
+                The point of using Lorem Ipsum is that it has a more-or-less
+                normal distribution of letters, as opposed to using.
+              </p>
+              <Button />
             </div>
           </div>
-          <div class="ab_right">
-            <h1>We Care About Your Dental Health</h1>
-            <p>
-              It is a long established fact that a reader will be distracted by
-              the readable content of a page when looking at its layout. The
-              point of using Lorem Ipsum is that it has a more-or-less normal
-              distribution of letters, as opposed to using.
-            </p>
-            <Button/>
+
+          <div class="abt_bottom_sec">
+            <div class="abt_sec sec1">
+              <div class="left">
+                <h1>Medical Service</h1>
+                <p>
+                  It is a long established fact that a reader will be distracted
+                  by the readable content of a page.
+                </p>
+              </div>
+              <div class="right">
+                <h1>01</h1>
+                <div class="icon">
+                  <img src={icon1} alt="" />
+                </div>
+              </div>
+            </div>
+            <div class="abt_sec sec2">
+              <div class="left">
+                <h1>24/7 Medicines</h1>
+                <p>
+                  It is a long established fact that a reader will be distracted
+                  by the readable content of a page.
+                </p>
+              </div>
+              <div class="right">
+                <h1>02</h1>
+                <div class="icon">
+                  <img src={icon2} alt="" />
+                </div>
+              </div>
+            </div>
+            <div class="abt_sec sec3">
+              <div class="left">
+                <h1>Best Doctor</h1>
+                <p>
+                  It is a long established fact that a reader will be distracted
+                  by the readable content of a page.
+                </p>
+              </div>
+              <div class="right">
+                <h1>03</h1>
+                <div class="icon">
+                  <img src={icon3} alt="" />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
